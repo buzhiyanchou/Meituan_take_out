775ebe80fbb1ad74f129d26000d43be2fa7ee60c
buzhiyanchou <508805622@qq.com>
2022-05-15 01:46:19 +0800

    [外卖]平台、店铺登录区分flag

--- src/main/java/com/meituan/reggie/controller/EmployeeController.java
+++ src/main/java/com/meituan/reggie/controller/EmployeeController.java
@@ -27,49 +27,82 @@ public class EmployeeController {
 
     /**
      * 员工登录
+     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
+    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee, Boolean flag) {
+        //平台后台登录true
+        if (Boolean.TRUE.equals(employee.getUsername())) {
+            //1、将页面提交的密码password进行md5加密处理
+            String password = employee.getPassword();
+            password = DigestUtils.md5DigestAsHex(password.getBytes());
 
-        //1、将页面提交的密码password进行md5加密处理
-        String password = employee.getPassword();
-        password = DigestUtils.md5DigestAsHex(password.getBytes());
+            //2、根据页面提交的用户名username查询数据库
+            LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(Employee::getUsername, employee.getUsername());
+            Employee emp = employeeService.getOne(queryWrapper);
 
-        //2、根据页面提交的用户名username查询数据库
-        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername,employee.getUsername());
-        Employee emp = employeeService.getOne(queryWrapper);
+            //3、如果没有查询到则返回登录失败结果
+            if (emp == null) {
+                return R.error("登录失败");
+            }
 
-        //3、如果没有查询到则返回登录失败结果
-        if(emp == null){
-            return R.error("登录失败");
+            //4、密码比对，如果不一致则返回登录失败结果
+            if (!emp.getPassword().equals(password)) {
+                return R.error("登录失败");
+            }
+
+            //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+            if (emp.getStatus() == 0) {
+                return R.error("账号已禁用");
+            }
+            //6、登录成功，将员工id存入Session并返回登录成功结果
+            request.getSession().setAttribute("employee", emp.getId());
+            return R.success(emp);
+
+            //店铺端登录传false
+        } else if (Boolean.FALSE.equals(flag)) {
+            //1、将页面提交的密码password
+            String password = employee.getPassword();
+
+            //2、根据页面提交的用户名username查询数据库
+            LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+            queryWrapper.eq(Employee::getUsername, employee.getUsername());
+            Employee emp = employeeService.getOne(queryWrapper);
+
+            //3、如果没有查询到则返回登录失败结果
+            if (emp == null) {
+                return R.error("登录失败");
+            }
+
+            //4、密码比对，如果不一致则返回登录失败结果
+            if (!emp.getPassword().equals(password)) {
+                return R.error("登录失败");
+            }
+
+            //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
+            if (emp.getStatus() == 0) {
+                return R.error("账号已禁用");
+            }
+
+            //6、登录成功，将员工id存入Session并返回登录成功结果
+            request.getSession().setAttribute("employee", emp.getId());
+            return R.success(emp);
         }
-
-        //4、密码比对，如果不一致则返回登录失败结果
-        if(!emp.getPassword().equals(password)){
-            return R.error("登录失败");
-        }
-
-        //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
-        if(emp.getStatus() == 0){
-            return R.error("账号已禁用");
-        }
-
-        //6、登录成功，将员工id存入Session并返回登录成功结果
-        request.getSession().setAttribute("employee",emp.getId());
-        return R.success(emp);
+        return R.error("登录失败");
     }
 
     /**
      * 员工退出
+     *
      * @param request
      * @return
      */
     @PostMapping("/logout")
-    public R<String> logout(HttpServletRequest request){
+    public R<String> logout(HttpServletRequest request) {
         //清理Session中保存的当前登录员工的id
         request.getSession().removeAttribute("employee");
         return R.success("退出成功");
@@ -77,12 +110,13 @@ public class EmployeeController {
 
     /**
      * 新增员工
+     *
      * @param employee
      * @return
      */
     @PostMapping
-    public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
-        log.info("新增员工，员工信息：{}",employee.toString());
+    public R<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
 
         //设置初始密码123456，需要进行md5加密处理
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
@@ -103,27 +137,28 @@ public class EmployeeController {
 
     /**
      * 员工信息分页查询
+     *
      * @param page
      * @param pageSize
      * @param name
      * @return
      */
     @GetMapping("/page")
-    public R<Page> page(int page,int pageSize,String name){
-        log.info("page = {},pageSize = {},name = {}" ,page,pageSize,name);
+    public R<Page> page(int page, int pageSize, String name) {
+        log.info("page = {},pageSize = {},name = {}", page, pageSize, name);
 
         //构造分页构造器
-        Page pageInfo = new Page(page,pageSize);
+        Page pageInfo = new Page(page, pageSize);
 
         //构造条件构造器
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
         //添加过滤条件
-        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
         //添加排序条件
         queryWrapper.orderByDesc(Employee::getUpdateTime);
 
         //执行查询
-        employeeService.page(pageInfo,queryWrapper);
+        employeeService.page(pageInfo, queryWrapper);
 
         return R.success(pageInfo);
     }
@@ -131,22 +166,23 @@ public class EmployeeController {
 
     /**
      * 店铺管理端后台管理端调用查询所有店铺信息分页查询
+     *
      * @param page
      * @param pageSize
      * @param name
      * @return
      */
     @GetMapping("/shopAdminPage")
-    public R<Page> shopPage(int page,int pageSize,String name){
-        log.info("page = {},pageSize = {},name = {}" ,page,pageSize,name);
+    public R<Page> shopPage(int page, int pageSize, String name) {
+        log.info("page = {},pageSize = {},name = {}", page, pageSize, name);
 
         //构造分页构造器
-        Page<Employee> pageInfo = new Page(page,pageSize);
+        Page<Employee> pageInfo = new Page(page, pageSize);
 
         //构造条件构造器
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper();
         //添加过滤条件
-        queryWrapper.like(StringUtils.isNotEmpty(name),Employee::getName,name);
+        queryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
         //添加排序条件
         queryWrapper.orderByDesc(Employee::getUpdateTime);
         //执行查询
@@ -159,15 +195,16 @@ public class EmployeeController {
 
     /**
      * 根据id修改员工信息
+     *
      * @param employee
      * @return
      */
     @PutMapping
-    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+    public R<String> update(HttpServletRequest request, @RequestBody Employee employee) {
         log.info(employee.toString());
 
         long id = Thread.currentThread().getId();
-        log.info("线程id为：{}",id);
+        log.info("线程id为：{}", id);
         //Long empId = (Long)request.getSession().getAttribute("employee");
         //employee.setUpdateTime(LocalDateTime.now());
         //employee.setUpdateUser(empId);
@@ -178,14 +215,15 @@ public class EmployeeController {
 
     /**
      * 根据id查询员工信息
+     *
      * @param id
      * @return
      */
     @GetMapping("/{id}")
-    public R<Employee> getById(@PathVariable Long id){
+    public R<Employee> getById(@PathVariable Long id) {
         log.info("根据id查询员工信息...");
         Employee employee = employeeService.getById(id);
-        if(employee != null){
+        if (employee != null) {
             return R.success(employee);
         }
         return R.error("没有查询到对应员工信息");
